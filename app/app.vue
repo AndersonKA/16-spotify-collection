0f91659e731c437b285ad25b392d1a96e758f435
Kae Anderson <andersonkaem@gmail.com>
2017-02-28 17:17:42 -0600

    started HTML

---
+++ app/app.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="app">
+    <div class="section">
+      <div class="container">
+        <div class="panel level">
+          <p class="panel-heading">
+            Repositories
+          </p>
+
+          <div class="panel-block level-left">
+          <div class="panel-block level-item">
+            bulma
+          </div>
+            <span class="panel-icon level-right">
+              <i class="fa fa-spotify level-item"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
