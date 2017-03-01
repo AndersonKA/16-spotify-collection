63221d4cf047fd221492049e198f74d12ee4b3aa
Kae Anderson <andersonkaem@gmail.com>
2017-03-01 09:31:53 -0600

    worked on JS

--- app/app.vue
+++ app/app.vue
@@ -2,34 +2,58 @@
   <div class="app">
     <div class="section">
       <div class="container">
-        <div class="panel level">
+
+        <div class="panel">
           <p class="panel-heading">
-            Repositories
+            Bastille
           </p>
 
-          <div class="panel-block level-left">
-          <div class="panel-block level-item">
-            bulma
-          </div>
-            <span class="panel-icon level-right">
-              <i class="fa fa-spotify level-item"></i>
-            </span>
+          <!--<div class="panel-block level-left">-->
+            <span class="panel-block level-item">Song
+
+
+          </span>
+          <song-item v-for="item in repositories" v-bind:track="item"></song-item>
+
           </div>
         </div>
       </div>
     </div>
-  </div>
+
+
 </template>
 
+
+
 <script>
+import SongItem from './song-item.vue';
 export default {
+  components: {
+    SongItem,
+  },
+
+
+  created() {
+    fetch('https://api.spotify.com/v1/search?query=bastille&type=track&offset=0&limit=20')
+      .then(response => response.json())
+      .then((track) => {
+        this.repositories = track;
+      });
+  },
   data() {
     return {
+      profile: {
+        artist: 'Bastille',
+        name: 'Pompeii'
+      },
+      repositories: [
+        { name: 'Pompeii' },
+        { name: 'Good Grief' },
+      ]
     };
   },
 
   methods: {
-
   },
 };
 </script>
---
+++ app/song-item.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="SongItem">
+      <img src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" alt="" class="SongItem__pic">
+        <h3 class="SongItem__artist">{{profile.artist}}</h3>
+        <h5 class="SongItem__name">{{profile.name}}</h5>
+        <span class="panel-icon level-right">
+          <i class="fa fa-spotify level-item"></i>
+        </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tracks'],
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
